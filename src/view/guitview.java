e7478b7ce51fd5e18da1524787e6c367644fe485
Eleanor Simon <simon5@email.arizona.edu>
2022-04-30 17:26:27 -0700

    hope this works

--- src/view/guitview.java
+++ src/view/guitview.java
@@ -244,7 +244,7 @@ public class guitview extends Application  {
 			            }
 			        });
 					// System.out.print(io);
-				 //push issue
+				 //push issue working on
 				Font font = Font.font("Times New Roman", 6);
 				map.get(i).setFont(font);
 				map.get(i).setStyle("-fx-border-style: solid;"+  "-fx-padding: 11px;" +  "-fx-border-color: lightgreen;" );
